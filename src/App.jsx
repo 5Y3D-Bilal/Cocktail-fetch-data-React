bd484e4473408d7dc8a89d719ee65f9a08677aae
】【爪尺 Ꮆ卂爪乇 尺卂几Ꮆ乇尺 <54587dfdd@gmail.com>
2023-10-16 22:20:50 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-
-import './App.css'
-import About from './pages/About';
-// import Coctail from './pages/coctail'
-// import Test from './pages/test';
 import {
   BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
-// import Test from './pages/test';
-// import Golo from './pages/yt';
+
 import Coctail from './pages/coctail';
-import Test from './pages/test';
+
 
 function App() {
 
@@ -19,9 +13,8 @@ function App() {
     <>
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test/>} />
-        <Route path="/about" element={<About/>} />
-        {/* <Route path="/users/*" element={<UserApp />} /> */}
+        <Route path="/" element={<Coctail/>} />
+        
       </Routes>
     </BrowserRouter>
     </>
